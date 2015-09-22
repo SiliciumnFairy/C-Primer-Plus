ca66cf62e0310d8baa6c92228e70f2ea60a545ff
Y Schrodinger <zzsb2b@gmail.com>
2015-09-22 15:16:15 +0800

    2105.9.22 15:16

---
+++ C Primer Plus/Test2.12-4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void For(void);
+void Which(void);
+int main(void)
+{
+	printf("For he`s a jolly good fellow\n");
+    For();
+	printf("For he`s a jolly good fellow\n");
+	Which();
+	return 0;
+}
+void For(void)
+{
+	printf("For he`s a jolly good fellow\n");
+	return 0;
+}
+void Which(void)
+{
+	printf("Which nobody can deny\n");
+	return 0;
+}
