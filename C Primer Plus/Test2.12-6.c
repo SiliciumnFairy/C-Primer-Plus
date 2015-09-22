5a697fafa110d9ce764b36f6e4d6418f3f477546
Y Schrodinger <zzsb2b@gmail.com>
2015-09-22 15:33:33 +0800

    2015.9.22 15:33

---
+++ C Primer Plus/Test2.12-6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void Smile(void);
+int main(void)
+{
+	Smile();
+	Smile();
+	Smile();
+	printf("\n");
+	Smile();
+	Smile();
+	printf("\n");
+	Smile();
+	printf("\n");
+	
+	return 0;
+}
+
+void Smile(void)
+{
+	printf("Smile!");
+	
+	return 0;
+}
