0768c16b0afda3f1458816b5593bd22158eb1e57
Y Schrodinger <zzsb2b@gmail.com>
2015-10-25 10:54:54 +0800

    2015.10.25 10:54

---
+++ C Primer Plus/Test5.11-5.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+int main(void) 
+{
+	int count, sum;
+	int la;
+
+	count = 0;
+	sum = 0;
+	
+	scanf("%d",&la);
+	while (count++ < la)
+	{
+		sum = sum + count;
+		printf("sum= %d\n", sum);
+	}
+	return 0;
+}
