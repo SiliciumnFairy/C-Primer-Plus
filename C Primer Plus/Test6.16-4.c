efeb1a54dcda0fbd9d59f3a177518010844d7551
Y Schrodinger <zzsb2b@gmail.com>
2015-11-04 21:15:52 +0800

    2015.11.4 21:15

---
+++ C Primer Plus/Test6.16-4.c
@@ -0,0 +1,25 @@
+/*blablablablalblabla*/
+#include <stdio.h>
+#pragma warning(disable:4996)
+
+int main(void)
+{
+	int i, j, k;
+	char letter;
+	printf("Please input a capital:");
+	scanf("%c", &letter);
+	
+	k = letter - 'A' + 1;
+
+	for (i = 0; i < k;i++)
+	{
+		for (j = 0; j<k - i; j++)
+			printf(" ");
+		for (j = 0; j <= i; j++)
+			printf("%c", 'A'+j);
+		for (j = i - 1; j >= 0; j--)
+			printf("%c", 'A'+j);
+		printf("\n");
+	}
+	return(0);
+}
